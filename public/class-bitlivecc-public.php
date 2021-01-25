b1411d07204df70e0f2f127857b1caef97256f34
dineshinau <dineshinau@gmail.com>
2021-01-26 00:10:12 +0530

    Added tutor dropdown for parent.

--- public/class-bitlivecc-public.php
+++ public/class-bitlivecc-public.php
@@ -21,6 +21,7 @@ class BITLIVECC_Public {
 	 */
 	public function __construct() {
 		add_filter( 'fep_menu_buttons', array( $this, 'remove_extra_menus' ), 99, 1 );
+		add_filter( 'fep_form_fields_after_process', array( $this, 'alter_message_recipients' ), 99, 2 );
 	}
 
 	/**
@@ -30,12 +31,73 @@ class BITLIVECC_Public {
 	 */
 	public function remove_extra_menus( $menu ) {
 		foreach ( $menu as $menu_key => $menu_data ) {
-			if ('newmessage' !== $menu_key){
-				unset($menu[$menu_key]);
+			if ( 'newmessage' !== $menu_key ) {
+				unset( $menu[ $menu_key ] );
 			}
 		}
 
 		return $menu;
+	}
+
+	/**
+	 * @param $fields
+	 * @param $where
+	 *
+	 * @return mixed
+	 */
+	public function alter_message_recipients( $fields, $where ) {
+		foreach ( $fields as $field_key => $field ) {
+			if ( 'message_to' === $field_key ) {
+				$fields[ $field_key ]['type'] = 'select';
+				$tutors                       = $this->get_recipients();
+
+				$fields[ $field_key ]['options'] = $tutors;
+				break;
+			}
+		}
+
+		return $fields;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_recipients() {
+		global $wpdb;
+		$recipients      = [];
+		$current_user_id = get_current_user_id();
+		if ( $current_user_id < 1 ) {
+			return $recipients;
+		}
+
+		$current_booking_table  = $wpdb->prefix . 'bookme_current_booking';
+		$customers_table        = $wpdb->prefix . 'bookme_customers';
+		$customer_booking_table = $wpdb->prefix . 'bookme_customer_booking';
+		$employee_table         = $wpdb->prefix . 'bookme_employee';
+
+		$current_user = wp_get_current_user();
+		$user_roles   = $current_user->roles;
+		if ( in_array( 'subscriber', $user_roles, true ) ) {
+			$parent_email = $current_user->user_email;
+			$customer_id  = $wpdb->get_var( "SELECT `id` FROM $customers_table WHERE `email`='$parent_email'" );
+			if ( $customer_id > 0 ) {
+				$booking_result = $wpdb->get_results( "SELECT DISTINCT(`booking_id`) FROM $customer_booking_table WHERE `customer_id`='$customer_id'", ARRAY_A );
+				$booking_ids    = wp_list_pluck( $booking_result, 'booking_id' );
+				if ( is_array( $booking_ids ) && count( $booking_ids ) > 0 ) {
+					$employee_result = $wpdb->get_results( "SELECT DISTINCT(`emp_id`) FROM $current_booking_table WHERE `id` IN (" . implode( ',', $booking_ids ) . ")", ARRAY_A );
+					$employee_ids    = wp_list_pluck( $employee_result, 'emp_id' );
+					if ( is_array( $employee_ids ) && count( $employee_ids ) > 0 ) {
+						$emp_result = $wpdb->get_results( "SELECT `id`,`name`, `email` FROM $employee_table WHERE `id` IN (" . implode( ',', $employee_ids ) . ")", ARRAY_A );
+						dk_pc_debug( $emp_result );
+						foreach ( $emp_result as $emp ) {
+							$recipients[ $emp['id'] ] = $emp['name'];
+						}
+					}
+				}
+			}
+		}
+
+		return $recipients;
 	}
 
 	/**
