1abdab51b26582b4e6ce207b24bc8c3b6d0922d1
dineshinau <dineshinau@gmail.com>
2021-01-27 04:22:33 +0530

    Added student list for tutor login.

--- public/class-bitlivecc-public.php
+++ public/class-bitlivecc-public.php
@@ -65,16 +65,18 @@ class BITLIVECC_Public {
 	public function get_recipients() {
 		global $wpdb;
 		$recipients      = [];
-		$recipients[0] = '-Select Recipient-';
+		$recipients[0]   = '-Select Recipient-';
 		$current_user_id = get_current_user_id();
 		if ( $current_user_id < 1 ) {
 			return $recipients;
 		}
 
-		$current_booking_table  = $wpdb->prefix . 'bookme_current_booking';
-		$customers_table        = $wpdb->prefix . 'bookme_customers';
-		$customer_booking_table = $wpdb->prefix . 'bookme_customer_booking';
-		$employee_table         = $wpdb->prefix . 'bookme_employee';
+		$current_booking_table      = $wpdb->prefix . 'bookme_current_booking';
+		$customers_table            = $wpdb->prefix . 'bookme_customers';
+		$customer_booking_table     = $wpdb->prefix . 'bookme_customer_booking';
+		$customer_booking_ref_table = $wpdb->prefix . 'bookme_customer_booking_ref';
+		$employee_table             = $wpdb->prefix . 'bookme_employee';
+		$bwlive_students            = $wpdb->prefix . 'bwlive_students';
 
 		$current_user = wp_get_current_user();
 		$user_roles   = $current_user->roles;
@@ -91,6 +93,32 @@ class BITLIVECC_Public {
 						$emp_result = $wpdb->get_results( "SELECT `id`,`name`, `email` FROM $employee_table WHERE `id` IN (" . implode( ',', $employee_ids ) . ")", ARRAY_A );
 						foreach ( $emp_result as $emp ) {
 							$recipients[ $emp['email'] ] = $emp['name'];
+						}
+					}
+				}
+			}
+		}
+
+		if ( in_array( 'lp_teacher', $user_roles, true ) ) {
+			$tutor_email     = $current_user->user_email;
+			$tutor_bookme_id = $wpdb->get_var( "SELECT `id` FROM $employee_table WHERE `email`='$tutor_email'" );
+			if ( $tutor_bookme_id > 0 ) {
+				$booking_result = $wpdb->get_results( "SELECT DISTINCT(`id`) as booking_id FROM $current_booking_table WHERE `emp_id`='$tutor_bookme_id'", ARRAY_A );
+				$booking_ids    = wp_list_pluck( $booking_result, 'booking_id' );
+				if ( is_array( $booking_ids ) && count( $booking_ids ) > 0 ) {
+					$student_result = $wpdb->get_results( "SELECT DISTINCT (`student_id`) FROM $customer_booking_ref_table WHERE `booking_id` IN (" . implode( ',', $booking_ids ) . ")", ARRAY_A );
+					$student_ids    = wp_list_pluck( $student_result, 'student_id' );
+					if ( is_array( $student_ids ) && count( $student_ids ) > 0 ) {
+						$std_id_str = '';
+						foreach ( $student_ids as $student_id ) {
+							$std_id_str .="'" . $student_id . "'";
+							if ( next( $student_ids ) ) {
+								$std_id_str .= ",";
+							}
+						}
+						$student_details = $wpdb->get_results( "SELECT `student_fname`, `student_lname`, `student_email` FROM $bwlive_students WHERE `student_id` IN ($std_id_str)", ARRAY_A );
+						foreach ( $student_details as $student ) {
+							$recipients[ $student['student_email'] ] = $student['student_fname'] . ' ' . $student['student_lname'];
 						}
 					}
 				}
